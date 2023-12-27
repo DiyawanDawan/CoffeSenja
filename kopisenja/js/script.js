58d6604e01f0fec9d752d623d4a3cd468d6bc1dd
DiyawanDawan <diyawan0@gmail.com>
2023-12-28 01:39:22 +0800

    Membuat Halaman Produk

--- kopisenja/js/script.js
+++ kopisenja/js/script.js
@@ -1,14 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nanbarNav = document.querySelector('.navbar-nav');
+    const navbarNav = document.querySelector('.navbar-nav');
     const hamburger = document.querySelector('#humberger-menu');
-    document.querySelector('#humberger-menu').onclick = (e) => {
-        e.stopPropagation();
-        nanbarNav.classList.toggle('active');
-    }
+    const searchForm = document.querySelector('.search-form');
+    const searchInput = document.querySelector('#search-box');
+    const searchButton = document.querySelector('#search-button');
 
-    document.addEventListener('click', function(e) {
-        if (!hamburger.contains(e.target) && !nanbarNav.contains(e.target)) {
-            nanbarNav.classList.remove('active');
+    const toggleActive = (element) => element.classList.toggle('active');
+    
+    searchButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleActive(searchForm);
+        searchInput.focus();
+    });
+
+    hamburger.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleActive(navbarNav);
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+            navbarNav.classList.remove('active');
+        }
+
+        if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+            searchForm.classList.remove('active');
         }
     });
 });
