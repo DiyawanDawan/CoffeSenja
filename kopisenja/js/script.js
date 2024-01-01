223adb86f6b5b4092b5d6204aa07004d06ff2319
DiyawanDawan <diyawan0@gmail.com>
2024-01-01 17:31:40 +0800

    Membuat Komit Card detail Modal dan Card CekOut

--- kopisenja/js/script.js
+++ kopisenja/js/script.js
@@ -4,9 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.querySelector('.search-form');
     const searchInput = document.querySelector('#search-box');
     const searchButton = document.querySelector('#search-button');
+    const shopingCard = document.querySelector('#shopping-card-button');
+    const activeShopingCard = document.querySelector('.shoping-card')
+
+    const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+    const itemDetailModal = document.querySelector('#item-detail-modal');
+    const modalClose = document.querySelector('.modal .close-icon');
+    
+    itemDetailButtons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log('Button clicked');
+            itemDetailModal.style.display = 'flex';
+        });
+    });
+
+    modalClose.addEventListener('click', (e) => {
+        itemDetailModal.style.display = 'none';
+        e.preventDefault();
+    })
 
     const toggleActive = (element) => element.classList.toggle('active');
-    
+
     searchButton.addEventListener('click', (e) => {
         e.preventDefault();
         toggleActive(searchForm);
@@ -17,14 +36,35 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
         toggleActive(navbarNav);
     });
+    
+    shopingCard.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleActive(activeShopingCard);
+    })
 
     document.addEventListener('click', (e) => {
+        let isItemDetailButton = false;
+
+        itemDetailButtons.forEach(btn => {
+            if (btn.contains(e.target)) {
+                isItemDetailButton = true;
+            }
+        });
+
+        if (!isItemDetailButton && !itemDetailModal.contains(e.target)) {
+            itemDetailModal.style.display = 'none';
+        }
+
         if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
             navbarNav.classList.remove('active');
         }
 
         if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
             searchForm.classList.remove('active');
+        }
+
+        if (!shopingCard.contains(e.target) && !activeShopingCard.contains(e.target)) {
+            activeShopingCard.classList.remove('active');
         }
     });
 });
